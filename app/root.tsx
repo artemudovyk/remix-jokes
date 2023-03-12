e2947e7d3c258aedd53726f224f35f15c458b368
Artem Udovyk <udovyk@gmail.com>
2023-03-12 16:04:06 +0200

    meta

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Links, LiveReload, Outlet, useCatch } from "@remix-run/react";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, useCatch } from "@remix-run/react";
 
 import globalStylesUrl from "./styles/global.css";
 import globalMediumStylesUrl from "./styles/global-medium.css";
@@ -24,11 +24,27 @@ export const links: LinksFunction = () => {
   ];
 };
 
+export const meta: MetaFunction = () => {
+  const description = `Learn Remix and laugh at the same time!`;
+  return {
+    charset: "utf-8",
+    description,
+    keywords: "Remix,jokes",
+    "twitter:image": "https://remix-jokes.lol/social.png",
+    "twitter:card": "summary_large_image",
+    "twitter:creator": "@remix_run",
+    "twitter:site": "@remix_run",
+    "twitter:title": "Remix Jokes",
+    "twitter:description": description,
+  };
+};
+
 function Document({ children, title = `Remix: So great, it's funny!` }: { children: React.ReactNode; title?: string }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <Meta />
         <title>{title}</title>
         <Links />
       </head>
